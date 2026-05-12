639c6ddc7769da9faa3848cc3da5a64bfc6e499a
Muhammad Usman Asrori <2313063@students.universitasmulia.ac.id>
2026-05-12 09:47:36 +0800

    initial commit project flutter

--- lib/main.dart
+++ lib/main.dart
@@ -31,40 +31,56 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: Project1(),
+    );
+  }
+}
+
+class Project2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+   return Scaffold();
+  }
+}
+
+class Project1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Youtube Music",style: TextStyle(fontSize: 40),),
+        centerTitle: true,
+        leading: Icon(Icons.account_circle, color: Colors.red, size: 50,),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.play_circle_outline_rounded, size: 40,color: Colors.red,),),
+          Padding(padding: EdgeInsets.all(10)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.mic_outlined, size: 40,color: Colors.red,))
+        ],
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+      ),
+      bottomNavigationBar: NavigationBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Youtube Music",style: TextStyle(fontSize: 40),),
-          centerTitle: true,
-          leading: Icon(Icons.account_circle, color: Colors.red, size: 50,),
-          actions: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.play_circle_outline_rounded, size: 40,color: Colors.red,),),
-            Padding(padding: EdgeInsets.all(10)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.mic_outlined, size: 40,color: Colors.red,))
-          ],
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
-        ),
-        bottomNavigationBar: NavigationBar(
-          backgroundColor: Colors.white,
-          indicatorColor: Colors.red,
-          destinations: const <Widget>[
-            NavigationDestination(
-              selectedIcon: Icon(Icons.home),
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Badge(child: Icon(Icons.notifications_sharp)),
-              label: 'Notifications',
-            ),
-            NavigationDestination(
-              icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
-              label: 'Messages',
-            ),
-          ],
-        ),
-      )
+        indicatorColor: Colors.red,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.notifications_sharp)),
+            label: 'Notifications',
+          ),
+          NavigationDestination(
+            icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
+            label: 'Messages',
+          ),
+        ],
+      ),
     );
   }
 }
